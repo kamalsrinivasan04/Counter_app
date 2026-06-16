a7d8b700f93b7ba5e9dda68ab5ac88a879053785
kamalsrinivasan04 <kamalnayans1864@gmail.com>
2026-06-16 17:11:40 +0530

    First commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import "./Counter.css";
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  useEffect(() => {
+  const handleKeyDown = (event) => {
+    if (event.key === "+") {
+      setCount((prevCount) => prevCount + 1);
+    }
+
+    if (event.key === "-") {
+      setCount((prevCount) => prevCount - 1);
+    }
+
+    if (event.key === "Backspace") {
+      setCount(0);
+    }
+  };
+
+  window.addEventListener("keydown", handleKeyDown);
+
+  return () => {
+    window.removeEventListener("keydown", handleKeyDown);
+  };
+}, []);
+
+  return (
+    <div className="counter-container">
+      <h1>React Counter App</h1>
+
+      <h2>Count : {count}</h2>
+
+      <div className="button-group">
+        <button onClick={increaseCount} >+</button>
+
+        <button onClick={decreaseCount}>-</button>
+
+        <button onClick={resetCount}>Reset</button>
+      </div>
+
+      {count > 0 && (
+        <p className="positive">
+          Positive Number
+        </p>
+      )}
+
+      {count < 0 && (
+        <p className="negative">
+          Negative Number
+        </p>
+      )}
+
+      {count === 0 && (
+        <p className="zero">
+          Count is Zero
+        </p>
+      )}
+    </div>
+  );
+}
+
+
+
+
+
+export default Counter;
